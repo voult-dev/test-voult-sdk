a8c33c067aba4c4e73f64fe3b9b78e1bcf958417
Samuel Olabode <solabode499@gmail.com>
2026-05-01 17:14:52 -0600

    Add signup endpoint using Voult SDK in index.js; update package.json for nodemon; remove unused auth client file.

--- auth/index.js
+++
@@ -1,9 +0,0 @@
-import { VoultClient } from 'voult-sdk';
-
-const client = new VoultClient({
-clientId: process.env.CLIENT_ID,
-  clientSecret: process.env.CLIENT_SECRET,
-  baseURL: process.env.BASE_URL
-});
-
-export default client;
--- index.js
+++ index.js
@@ -2,12 +2,27 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.port || 2000;
+import { VoultClient } from 'voult-sdk';
+import { signUpWithEmailAndPassword } from 'voult-sdk';
 
+const client = new VoultClient({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  baseURL: process.env.BASE_URL
+});
 
+app.post('/signup-email', async (req, res) => {
+    const { email, password } = req.body;
+    const { user, token } = await signUpWithEmailAndPassword(email, password, client);
+    res.json({ user, token });
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
+
+
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
