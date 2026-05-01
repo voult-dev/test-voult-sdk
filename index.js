98e7d796bf3ea256da99a57d2b03bb90e06ab819
Samuel Olabode <solabode499@gmail.com>
2026-05-01 17:04:29 -0600

    Add Express and Dotenv for server setup; create basic server in index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.port || 2000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
