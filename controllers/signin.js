69ee39435d813f8cb5ee2f0f7ee2e679d9f1c4e0
Samuel Olabode <solabode499@gmail.com>
2026-05-01 22:54:25 -0600

    Add signin controller methods for email and username authentication; update routes to handle new endpoints.

---
+++ controllers/signin.js
@@ -0,0 +1,16 @@
+const client = require('../config/client');
+const { signInWithEmailAndPassword,signInWithUsernameAndPassword } = require('voult-sdk');
+
+module.exports.signinWithEmailAndPassword = async (req, res) => {
+    console.log(req.body);
+    const { email, password } = req.body;
+    const { user, token } = await signInWithEmailAndPassword(email, password, client);
+    res.json({ user, token });
+};
+
+module.exports.signinWithUsernameAndPassword = async (req, res) => {
+    console.log(req.body);
+    const { username, password } = req.body;
+    const { user, token } = await signInWithUsernameAndPassword(username, password, client);
+    res.json({ user, token });
+};
--- routes/signin.js
+++ routes/signin.js
@@ -3,4 +3,7 @@ const router = express.Router();
 
 const controller = require('../controllers/signin');
 
+router.post('/email', controller.signinWithEmailAndPassword);
+router.post('/username', controller.signinWithUsernameAndPassword);
+
 module.exports = router;
