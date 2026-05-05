055bb39883354065c4e819bd59616c603c81eccc
Samuel Olabode <solabode499@gmail.com>
2026-05-04 18:27:17 -0600

    Add session management and view rendering setup in index.js. Integrate express-session, connect-flash, ejs-mate, and method-override for improved user experience and functionality. #1

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+const session = require('express-session');
+
+const sessionConfig = require('../config/session');
+
+const methodOverride = require('method-override');
+const flash = require('connect-flash');
+
+const path = require('path');
+const ejsMate = require('ejs-mate');
+
 app.use(express.json());
 app.use(express.urlencoded({ 
   extended: true,
@@ -10,7 +20,27 @@ app.use(express.urlencoded({
 
 const signinRoutes = require('../routes/signin');
 const signupRoutes = require('../routes/signup');
-const userRoutes = require('../routes/user')
+const userRoutes = require('../routes/user');
+
+app.use(session(sessionConfig));
+app.use(flash());
+
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine', 'ejs');
+
+app.engine('ejs', ejsMate);
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.use(methodOverride('_method'));
+
+app.use((req, res, next) => {
+  res.locals.success = req.flash('success');
+  res.locals.error = req.flash('error');
+  res.locals.info = req.flash('info');
+  res.locals.currentUser = req.user;
+  next();
+});
 
 app.use('/signin', signinRoutes);
 app.use('/signup', signupRoutes);
