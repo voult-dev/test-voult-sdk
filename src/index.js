62b42618fd9d440e49db34cdff9fc4e625ff06ed
Samuel Olabode <solabode223@gmail.com>
2026-07-11 09:44:46 -0600

    Update Voult SDK version to 0.0.3, remove local SDK reference, and comment out unused error handling route in index.js for improved code clarity.

--- src/index.js
+++ src/index.js
@@ -56,9 +56,9 @@ app.get('/', (req, res) => {
   res.render('home', { title: 'Voult SDK test app' });
 });
 
-app.all(/(.*)/, (req, res, next) => {
-  next(new ExpressError('Page not found', 404))
-});
+// app.all(/(.*)/, (req, res, next) => {
+//   next(new ExpressError('Page not found', 404))
+// });
 
 app.use((err, req, res, next)=>{
   const {statusCode = 500} = err;
