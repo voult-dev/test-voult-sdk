28cdd55c7fb3d940d1a2969a1fa8262d6abe827c
Samuel Olabode <solabode223@gmail.com>
2026-07-11 10:08:21 -0600

    Remove deprecated files and configurations, including .gitignore, package.json, package-lock.json, and various controller and route files, to streamline the codebase and eliminate unused components. This cleanup enhances maintainability and reduces clutter in the project structure.

--- utils/catchAsync.js
+++
@@ -1,9 +0,0 @@
-module.exports = func => {
-    return (req, res, next) => {
-        if (typeof func !== 'function') {
-            return next(new Error('catchAsync received a non-function'));
-        }
-
-        Promise.resolve(func(req, res, next)).catch(next);
-    };
-};
--- utils/expressError.js
+++
@@ -1,9 +0,0 @@
-class ExpressError extends Error{
-    constructor(message, statusCode){
-        super(message);
-        this.message = message;
-        this.statusCode = statusCode;
-    }
-};
-
-module.exports = ExpressError;
